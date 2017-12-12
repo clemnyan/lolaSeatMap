ca834f2b45214dab3a9f95af3f23b0cce17a70b0
clemnyan <clemnyan@gmail.com>
2017-12-11 20:01:40 -0500

    NavBar component added

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { } from 'react-router-dom';
+
+const NavBar = (props) => {
+  return (
+    <nav>
+      <div className="NAV">Welcome to App: Lola Seats</div>
+    </nav>
+  );
+};
+
+export default NavBar;
