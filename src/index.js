793b2856ef95e8a3519ecf4ebecbbf34792ce234
clemnyan <clemnyan@gmail.com>
2017-12-11 22:21:15 -0500

    updated main index file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import React from 'react';
+import App from './components/App';
+
 
 import reducers from './reducers';
 
-import App from './components/App';
-
-// this creates the store with the reducers, and does some other stuff to initialize devtools
 const store = createStore(reducers, {}, compose(
-  applyMiddleware(),
+  applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f,
+
 ));
 
+
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={store} >
     <App />
   </Provider>
-, document.getElementById('main'));
+  , document.getElementById('main'));
